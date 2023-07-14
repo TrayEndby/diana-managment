0a293d8a2d4e2b7853fd67f5bef964b32f7ec342
trayendby <trayjenbaj@gmail.com>
2023-07-14 12:12:00 -0700

    Cleanup

--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
-
 for Y in {2023..2023}
 do
-  for M in {7..7}
+  for M in {07..7}
   do
-    for D in {1..15}
+    for D in {01..14}
     do
       for i in {01..12}
       do
-        echo "$i on $M/$D/$Y"
+        echo "$i on $M/$D/$Y" > commit.md
         export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
         export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
-        git add . -f
+        git add commit.md -f
         git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
       done
     done
   done
 done
+
 git push origin master
+echo "" > commit.md
 git commit -am "Cleanup"
 git push origin master
